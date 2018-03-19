fab60b020fa9a156804ca7e0633a8d1730a75678
vzhurav <vlad.zurs2@gmail.com>
2018-03-19 12:36:38 +0300

    db changes

--- vega/Models/Make.cs
+++ vega/Models/Make.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Security.AccessControl;
 using System.Threading.Tasks;
 
 namespace vega.Models
 {
+    [Table("Models")]
     public class Make
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
         public ICollection<Model> Models { get; set; }
